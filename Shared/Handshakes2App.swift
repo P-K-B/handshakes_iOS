a9699e72086ae7fd65d74ad71fc1e8cd1bfc0c5a
FREEKILLER201020 <36477166+FREEKILLER201020@users.noreply.github.com>
2022-05-17 21:09:53 +0300

    Initial Commit

---
+++ Shared/Handshakes2App.swift
@@ -0,0 +1,17 @@
+//
+//  Handshakes2App.swift
+//  Shared
+//
+//  Created by Kirill Burchenko on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Handshakes2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
